90a7fee5d3cb59ac0c91f0129732454d5ffd210c
moregorenine <moregorenine@gmail.com>
2022-03-01 13:13:06 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/MJTnrbA-npF7lRp24_WfO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f41b2aee5daff2b7.css","static/chunks/pages/index-8cf30008ef1b0880.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
